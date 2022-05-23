902fff9bb054fd19acca44a000e0350171d79cf7
TatsukiYamada <105026743+taaaaaaachu@users.noreply.github.com>
2022-05-23 08:09:53 +0000

    pagenation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "c7b019509a5b4350a53a38070cc22916.vfs.cloud9.us-east-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
 end
